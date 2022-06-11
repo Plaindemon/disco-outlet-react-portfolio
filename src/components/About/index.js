8554601d47cccdd0ef104e976e339befd396c43b
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-06-10 18:52:34 -0600

    about section updated slightly

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -19,7 +19,7 @@ function About() {
         <div className="about-modal">
           <h1 className="about-header">Who am I?</h1>
           <img src={coverImage} className="my-2 about-img" style={{ width: "20%" }} alt="cover" />
-          <p className="about-paragraph"> Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo. Nunc mattis enim ut tellus elementum sagittis vitae. Amet purus gravida quis blandit turpis. A lacus vestibulum sed arcu non. Enim neque volutpat ac tincidunt vitae semper quis lectus nulla. Platea dictumst vestibulum rhoncus est. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Sapien et ligula ullamcorper malesuada proin libero. Aliquam ut porttitor leo a diam sollicitudin tempor id. Pharetra magna ac placerat vestibulum lectus mauris ultrices. Facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris. Ipsum suspendisse ultrices gravida dictum fusce ut placerat. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Tincidunt lobortis feugiat vivamus at augue eget arcu.</p>
+          <p className="about-paragraph"> Full stack web developer with experience in HTML, javascript, react as well as experience outside the web development field in anthropology, psychology, science such as biology and chemistry, as well as experience in sales, customer service, art/ceramics and many other fields.</p>
         </div>
         <div>
           
